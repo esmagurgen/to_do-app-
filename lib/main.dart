7b7e54fd7ad5b6fcdfcb093c39aadcff0b250679
esmagurgen <esmagrgn01@gmail.com>
2026-09-24 20:08:35 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+//Flutter'da elevation, bir widget'ın ekrandan ne kadar yüksekte (havada) durduğunu belirleyen özelliktir. Arayüze 3 boyutlu bir derinlik katar ve widget'ın arkasına gölge (shadow) ekler.
+//Flutter'da leading, bir satırın veya başlığın en başında (sol tarafında) yer alan ögeyi belirleyen parametredir.
+//Container sadece bir tane child kabul eder.Daha falzası için Column veya Row içinde childrem:[] kullanılır 
 void main() {
   runApp(MyWidget());
 }
@@ -12,7 +14,15 @@ class MyWidget extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home:Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Text("my app bar"),
+          elevation: 0,
+          leading:Icon(Icons.menu)  ,
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+          ),
         backgroundColor: Colors.blue,
+
         body:Center(
           child:Container(
           height: 300,
@@ -20,12 +30,12 @@ class MyWidget extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.cyanAccent,
             borderRadius: BorderRadius.circular(20),
-
           ), 
-         child: Text(
-          "HELLO WORLD",
-          style: TextStyle(color: Colors.white,fontSize: 28),
-          ),
+          child:Icon(
+            Icons.favorite,
+           color: Colors.indigoAccent,
+           size:64
+            ),
         ),
         ),
 
