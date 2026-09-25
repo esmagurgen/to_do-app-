2cf640e0e6ab4bdb7dd34230f60009d9b00d8c80
esmagurgen <esmagrgn01@gmail.com>
2026-09-25 07:56:29 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,38 @@
 import 'package:flutter/material.dart';
+
 //Flutter'da elevation, bir widget'ın ekrandan ne kadar yüksekte (havada) durduğunu belirleyen özelliktir. Arayüze 3 boyutlu bir derinlik katar ve widget'ın arkasına gölge (shadow) ekler.
 //Flutter'da leading, bir satırın veya başlığın en başında (sol tarafında) yer alan ögeyi belirleyen parametredir.
-//Container sadece bir tane child kabul eder.Daha falzası için Column veya Row içinde childrem:[] kullanılır 
+//Flutter'da mainAxisAlignment, bir Row (satır) veya Column (sütun) içindeki elemanların ana eksen (main axis) üzerinde nasıl hizalanacağını ve aralarındaki boşlukların nasıl dağıtılacağını belirleyen özelliktir.
+//Flutter'da Expanded, bir Row, Column veya Flex içinde bulunan elemanın kalan tüm boş alanı kaplamasını sağlayan widget'tır.
+//İçine koyduğunuz widget'ı esnetir ve ekranda boş yer bırakmayacak şekilde doldurur
+//Container sadece bir tane child kabul eder.Daha falzası için Column veya Row içinde childrem:[] kullanılır
 void main() {
   runApp(MyWidget());
 }
 
 class MyWidget extends StatelessWidget {
-   const MyWidget({super.key});
+ MyWidget({super.key});
 
+  List names = ["Mitch","Sharon","Vince"];
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.cyan,
           title: Text("my app bar"),
           elevation: 0,
-          leading:Icon(Icons.menu)  ,
+          leading: Icon(Icons.menu),
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-          ),
-        backgroundColor: Colors.blue,
-
-        body:Center(
-          child:Container(
-          height: 300,
-          width: 300, 
-          decoration: BoxDecoration(
-            color: Colors.cyanAccent,
-            borderRadius: BorderRadius.circular(20),
-          ), 
-          child:Icon(
-            Icons.favorite,
-           color: Colors.indigoAccent,
-           size:64
-            ),
         ),
+        //ListView, yazılımda (özellikle Flutter, Android, React Native gibi mobil ve web geliştirme teknolojilerinde) birden fazla ögeyi dikey veya yatay olarak sıralayan ve ekrana sığmayan ögeleri kaydırmayı (scroll) sağlayan bir liste bileşenidir (widget/component).
+        body: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) =>
+              ListTile(title: Text(names[index])),
         ),
-
       ),
-      );
+    );
   }
 }
